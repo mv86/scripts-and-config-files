5b42f075a450f1c7cc72faf86fd3085753ca98f8
Max Veasey <mveasey87@gmail.com>
2018-01-13 08:00:42 +0000

    added initial config files and battery script

---
+++ config_files/rc.lua
@@ -0,0 +1,29 @@
+require("termit.colormaps")
+require("termit.utils")
+
+defaults = {}
+defaults.windowTitle = 'Echo'
+defaults.startMaximized = true
+defaults.hideTitlebarWhenMaximized = true
+defaults.tabName = 'Terminal'
+defaults.encoding = 'UTF-8'
+defaults.wordCharExceptions = '- .,_/'
+defaults.font = 'Monospace 18'
+--defaults.foregroundColor = 'gray'
+--defaults.backgroundColor = 'black'
+defaults.showScrollbar = true
+defaults.hideSingleTab = true
+defaults.hideTabbar = true
+defaults.showBorder = true
+defaults.hideMenubar = true
+defaults.fillTabbar = true
+defaults.scrollbackLines = 4096
+defaults.geometry = '80x24'
+defaults.allowChangingTitle = false
+--defaults.backspaceBinding = 'AsciiBksp'
+--defaults.deleteBinding = 'AsciiDel'
+defaults.cursorBlinkMode = 'BlinkOn'
+defaults.cursorShape = 'Ibeam'
+defaults.tabPos = 'Right'
+defaults.colormap = termit.colormaps.delicate
+setOptions(defaults)
